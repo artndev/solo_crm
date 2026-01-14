4e4375f33643a53f7c42d5ace20ff55d975b147a
artndev <97910780+artndev@users.noreply.github.com>
2026-01-14 14:26:45 +0300

    Initial commit

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,47 +1,13 @@
-import { ClerkProvider } from '@clerk/clerk-expo'
-import { useFonts } from 'expo-font'
-import { SplashScreen, Stack } from 'expo-router'
-import { useEffect } from 'react'
+import { Stack } from 'expo-router'
 import 'react-native-reanimated'
 import './global.css'
 
-SplashScreen.preventAutoHideAsync()
-
 export const RootLayout = () => {
-  const [loaded] = useFonts({
-    'Jakarta-Bold': require('../assets/fonts/PlusJakartaSans-Bold.ttf'),
-    'Jakarta-ExtraBold': require('../assets/fonts/PlusJakartaSans-ExtraBold.ttf'),
-    'Jakarta-ExtraLight': require('../assets/fonts/PlusJakartaSans-ExtraLight.ttf'),
-    'Jakarta-Light': require('../assets/fonts/PlusJakartaSans-Light.ttf'),
-    'Jakarta-Medium': require('../assets/fonts/PlusJakartaSans-Medium.ttf'),
-    'Jakarta-Regular': require('../assets/fonts/PlusJakartaSans-Regular.ttf'),
-    'Jakarta-SemiBold': require('../assets/fonts/PlusJakartaSans-SemiBold.ttf'),
-  })
-
-  useEffect(() => {
-    if (!loaded) {
-      return
-    }
-
-    SplashScreen.hideAsync()
-  }, [loaded])
-
-  if (!loaded) {
-    return null
-  }
-
-  if (!process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY) {
-    throw new Error('EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY is not in .env')
-  }
-
   return (
-    <ClerkProvider>
-      <Stack>
-        <Stack.Screen name="index" options={{ headerShown: false }} />
-        <Stack.Screen name="(auth)" options={{ headerShown: false }} />
-        <Stack.Screen name="(root)" options={{ headerShown: false }} />
-      </Stack>
-    </ClerkProvider>
+    <Stack>
+      <Stack.Screen name="index" options={{ headerShown: false }} />
+      <Stack.Screen name="(auth)/welcome" options={{ headerShown: false }} />
+    </Stack>
   )
 }
 
--- app/index.tsx
+++ app/index.tsx
@@ -1,12 +1,11 @@
-import { useAuth } from '@clerk/clerk-expo'
 import { Redirect } from 'expo-router'
 
 const Index = () => {
-  const { isSignedIn } = useAuth()
+  // const { isSignedIn } = useAuth()
 
-  if (isSignedIn) {
-    return <Redirect href="/(root)/(tabs)/home" />
-  }
+  // if (isSignedIn) {
+  //   return <Redirect href="/(root)/(tabs)/home" />
+  // }
 
   return <Redirect href="/(auth)/welcome" />
 }
